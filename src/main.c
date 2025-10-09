197432314ce3acd30d95adddc45ccc0439fe56a1
Giovanni Chacon De Vito <giovanni.devito@tutamail.com>
2025-10-09 19:57:24 +0200

    REPL

--- src/main.c
+++ src/main.c
@@ -2,18 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+int main(int argc, char *argv[])
+{
+  while (1)
+  {
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  printf("$ ");
+    printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = '\0';
-  
-  printf("%s: command not found\n", input);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    input[strcspn(input, "\n")] = '\0';
+
+    printf("%s: command not found\n", input);
+  }
 
   return 0;
 }
