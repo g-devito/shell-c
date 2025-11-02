def27d241953125610cdbb28476a2e11ab910c41
Giovanni Chacon De Vito <giovanni.devito@tutamail.com>
2025-11-02 12:54:11 +0100

    add max input const

--- src/main.c
+++ src/main.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  const int MAX_INPUT_SIZE = 100;
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
 
   // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+  char input[MAX_INPUT_SIZE];
+  fgets(input, MAX_INPUT_SIZE, stdin);
 
   return 0;
 }
